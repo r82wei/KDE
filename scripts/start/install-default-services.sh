baae4a70373ff81672402399e7e7f65233fa94c0
Maxime <machanwei@gmail.com>
2025-03-19 16:21:23 +0800

    feat: update local-path-storage installation and configuration & hide notes of ingress installtion

--- scripts/start/install-default-services.sh
+++ scripts/start/install-default-services.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# fix local-path-storage configmap
+kubectl patch configmap local-path-config -n local-path-storage --type merge -p '{"data": {"config.json": "{\n  \"sharedFileSystemPath\": \"/opt/local-path-provisioner\"\n}"}}'
+kubectl -n local-path-storage rollout restart deployment local-path-provisioner
+kubectl wait --for=condition=ready pod -l app=local-path-provisioner -n local-path-storage --timeout=300s
 
-# Install local-path-storage
-kubectl apply -f ./scripts/start/yaml/local-path-storage.yaml
+# Install local-path storage class
+kubectl apply -f - <<EOF
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: local-path
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+provisioner: rancher.io/local-path
+parameters:
+  archiveOnDelete: 'false'
+  pathPattern: "{{ .PVC.Namespace }}/{{ .PVC.Name }}"
+reclaimPolicy: Retain
+volumeBindingMode: Immediate
+EOF
 
 
 # Install ingress-nginx
@@ -10,7 +27,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx \
     -n ingress-nginx --create-namespace \
-    --wait \
+    --hide-notes \
     --set controller.service.type=NodePort \
     --set controller.config.enable-access-log=true \
     --set controller.service.nodePorts.http=30080 \
